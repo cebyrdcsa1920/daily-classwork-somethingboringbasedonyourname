6902cf46bd5c17f8760cb16917fe36300720f00f
somethingboringbasedonyourname <lpskiitfit@gmail.com>
2019-09-04 13:47:33 +0000

    Create Car.java and its no args constructor

---
+++ week4/Car.java
@@ -0,0 +1,17 @@
+import.java.awt.Color;
+
+public class Car
+{
+    private String model;
+    private Color color;
+    private int numPassengers;
+    private double amountOfGas;
+
+    public Car()
+    {
+        model = "Mustang";
+        color = Color.YELLOW;
+        numPassengers = 2;
+        amountOfGas = 1000;
+    }
+}
