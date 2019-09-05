066995002484de0725709bd024ed8a6b89e91825
somethingboringbasedonyourname <lpskiitfit@gmail.com>
2019-09-05 14:16:35 +0000

    make a person

---
+++ week4/Person.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+
+public class Person
+{
+    private String gender;
+    private Color color;
+    private Color hairColor;
+    private String hairLength;
+    int lengthInches;
+
+    public Person()
+    {
+        gender = "Female";
+        color = Color.YELLOW;
+        hairLength = "Long";
+        lengthInches = 0;
+    }
+
+    public Person(String gender, Color color, Color hairColor, String hairLength)
+    {
+        this.gender = gender;
+    }
+    /*
+    public Person()
+    {
+        gender = "Male";
+        color = Color.BLACK;
+        hairLength = "Short";
+        lengthInches = 18;
+    }
+    public Person()
+    {
+        gender = "War Helicopter";
+        color = Color.PINK;
+        hairLength = "Bald";
+        lengthInches = 2000;
+    }
+    */
+}
