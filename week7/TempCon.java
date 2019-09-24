2c7f45733be043acce12f3bdca4d7179547aa596
somethingboringbasedonyourname <lpskiitfit@gmail.com>
2019-09-24 14:14:12 +0000

    Convert temperatures

--- week7/TempCon.java
+++ week7/TempCon.java
@@ -3,9 +3,9 @@ public class TempCon
     private double temp;
     private boolean isCelsius;
 
-    public TemperatureConverter(double celsius)
+    public TempCon(double celsius)
     {
-        temperature = celsius
+        temp = celsius;
         isCelsius = true;
     }
     public void convertToF()
@@ -14,11 +14,21 @@ public class TempCon
         if (isCelsius)
         {
             temp = temp * 9 / 5 + 32;
+            isCelsius = false;
         }
+    }
+
+    public void convertToC()
+    {
+     if (!isCelsius)
+     {
+        temp = (temp - 32) * 5/9;
+        isCelsius = true;
+     }
     }
     public String toString()
     {
-        String result = "The current temperature is " + temp + " degrees";
+        String result = "The current temperature is " + temp + " degrees ";
         if (isCelsius)
         {
             result += "Celsius";
@@ -27,4 +37,16 @@ public class TempCon
         {
             result += "Fahrenheit";
         }
+        return result;
     }
+
+    public static void main(String[] args)
+    {
+        TempCon myTemp = new TempCon(25.6);
+        System.out.println(myTemp);
+        myTemp.convertToF();
+        System.out.println(myTemp);
+        myTemp.convertToC();
+        System.out.println(myTemp);
+    }
+}
